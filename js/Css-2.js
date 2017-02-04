a46317767c39d307f5adc8ee42d939ce02d03d81
Pavel <pavelyukhnevich@yandex.ru>
2017-02-04 22:35:05 +0300

    'qwe'

---
+++ js/Css-2.js
@@ -0,0 +1,9 @@
+;document.addEventListener("DOMContentLoaded", Css);
+function Css() {
+    var ls = document.createElement('link');
+    ls.rel="stylesheet";
+    ls.href= "style.css";
+    document.getElementsByTagName('head')[0].appendChild(ls);
+};
+(function(w,g){w['GoogleAnalyticsObject']=g; w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
